d74e019027ae3c77885ef405b098f86f05c9a94f
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-12-28 12:39:52 -0500

    funcion implementada de limpiar todo

--- puntoDeVenta.js
+++ puntoDeVenta.js
@@ -93,5 +93,16 @@ limpiar=function(){
         Dejar todos los textos de los montos con el valor 0.0
         Si funciona, hacer un commit
      */
+
+        //mostrarTextoEnCaja("txtProducto", "");
+        document.getElementById("txtProducto").value = "";
+        mostrarTextoEnCaja("txtCantidad", 0);
+        mostrarTextoEnCaja("txtPrecio", 0.0);
+        mostrarTextoEnCaja("txtPorcentajeDescuento", 0);
+        mostrarTexto("lblSubtotal", 0.0);
+        mostrarTexto("lblDescuento", 0.0);
+        mostrarTexto("lblValorIVA", 0.0);
+        mostrarTexto("lblTotal", 0.0);
+        mostrarTexto("lblResumen", "");
 }
 /* SI TODO FUNCIONA, HACER UN PUSH */
