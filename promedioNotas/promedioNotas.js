a0bd872f6efb8f76a1441cdd21bc42d759c17078
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-12-28 15:01:28 -0500

    Funcion mostrar imagen de exito o fracaso!

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -7,4 +7,10 @@ calcularPromedioNotas = function () {
     let promedio = calcularPromedio(nota1, nota2, nota3);
 
     mostrarTexto("txtResultado",promedio);
+
+    if(promedio > 7){
+        mostrarImagen("imagen", "./imagenes/exito.gif");
+    }else{
+        mostrarImagen("imagen", "./imagenes/fracaso.gif");
+    }
 }
