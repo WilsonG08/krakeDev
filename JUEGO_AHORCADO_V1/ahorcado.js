9c9975a297348333227fa5aed02459d86b967494
WilsonG08 <wilson.guayanay@epn.edu.ec>
2024-01-08 07:30:22 -0500

    PASO 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -4,3 +4,18 @@ esMayuscula = function(caracter) {
     const ascii = caracter.charCodeAt(0);
     return ascii >= 65 && ascii <= 90;
 }
+
+
+let palabraSecreta = '';
+
+guardarPalabra = function () {
+    let input = document.getElementById('txtSecreta');
+    let palabra = input.value;
+
+    if (palabra.length === 5 && palabra === palabra.toUpperCase()) {
+        palabraSecreta = palabra;
+        console.log(palabraSecreta);
+    } else {
+        alert('Debe ingresar una palabra de 5 letras mayúsculas');
+    }
+}
