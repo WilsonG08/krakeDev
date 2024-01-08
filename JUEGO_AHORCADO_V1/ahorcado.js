a30bf383f8a35a5065b88abf84f571554ca5a5f7
WilsonG08 <wilson.guayanay@epn.edu.ec>
2024-01-08 08:20:39 -0500

    Finalizado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,6 +1,10 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 
 let palabraSecreta = '';
+let intentos = 0;
+let coincidencias = 0;
+let errores = 0;
+
 
 esMayuscula = function(caracter) {
     const ascii = caracter.charCodeAt(0);
@@ -37,14 +41,35 @@ validar = function(letra) {
             letrasEncontradas++;
         }
     }
+
+    if (letrasEncontradas > 0) {
+        coincidencias += letrasEncontradas;
+    } else {
+        errores++;
+        alert('LA LETRA NO ES PARTE DE LA PALABRA');
+        mostrarAhorcado();
+    }
 }
+
 
 ingresarLetra = function() {
     let letra = recuperarTexto('txtLetra');
     
     if (esMayuscula(letra)) {
+        intentos++;
         validar(letra);
     } else {
         alert('SOLO SE ACEPTAN MAYUSCULAS');
     }
+
+    if (coincidencias === 5) {
+        mostrarImagen('ahorcadoImagen', 'ganador.gif');
+    } else if (intentos === 10) {
+        mostrarImagen('ahorcadoImagen', 'gameOver.gif');
+    }
+}
+
+mostrarAhorcado = function() {
+    let rutaImagen = 'Ahorcado_0' + errores + '.png';
+    mostrarImagen('ahorcadoImagen', rutaImagen);
 }
