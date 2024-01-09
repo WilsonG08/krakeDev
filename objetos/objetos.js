402c1d04c85a8d7a8747950e348511d30ae26daf
WilsonG08 <wilson.guayanay@epn.edu.ec>
2024-01-09 11:57:19 -0500

    Funcionamiento de pantalla

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -39,3 +39,73 @@ crearProducto = function(){
         console.log("Producto 2 tiene mayor stock");
     }
 }
+
+modificarAtributos = function(){
+    let cuenta = {
+        numero : "342567385",
+        saldo : 0.0,
+    }
+
+    cuenta.saldo = 100;
+    cuenta.saldo += 10;
+    console.log(cuenta.saldo);
+}
+
+crearCliente = function(){
+    let cliente = {
+        cedula : "1234567890",
+        nombre : "Juan"
+    }
+
+    //cliente.nombre = "Juanito";
+    // cliente.nombr = "xxxx"
+
+    let cliente1 = {}
+    cliente1.nombre = "ROmero";
+    cliente1.apellido = "Salcedo";
+    cliente1.cedula = "256358906";
+}
+
+
+probarIncrementoSaldo = function(){
+    let cta={
+        numero : "22333232",
+        saldo : 34.00
+    };
+    incrementarSaldo(cta, 100);
+    console.log(cta.saldo);
+}
+
+probarDeterminarMayor = function(){
+    let mayor;
+
+    let per1 = {
+        nombre : "Daniel",
+        edad : 45
+    }
+    let per2 = {
+        nombre : "Luis",
+        edad : 18
+    }
+
+    mayor = determinarMayor(per1, per2);
+
+    if(mayor != null){
+        console.log("El mayor es: " + mayor.nombre);
+    }
+}
+
+incrementarSaldo = function(cuenta, monto){
+    cuenta.saldo += monto;
+}
+
+determinarMayor = function(persona1, persona2){
+    
+    if (persona1.edad > persona2.edad) {
+        return persona1;
+    }else if (persona2.edad > persona1.edad) {
+        return persona2;
+    }else{
+        return null;
+    }
+}
